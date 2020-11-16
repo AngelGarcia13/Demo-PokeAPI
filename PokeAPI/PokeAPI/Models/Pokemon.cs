496f0474a7fa086c6b2523b8af923c53db0d6e80
Angel Garcia <argarcia@hiberus.com>
2020-11-16 09:49:57 +0100

    Class 0 and 1 Completed

---
+++ PokeAPI/PokeAPI/Models/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PokeAPI.Models
+{
+    public class Pokemon
+    {
+        [Required]
+        public string Code { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Type1 { get; set; }
+
+        public string Type2 { get; set; }
+    }
+}
